55a84d64230d5ca5517b548df626f76f224a7e04
Saleh Bichara Senoussi <sabbay026@gmail.com>
2023-05-13 18:52:42 +0000

    update handle

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -133,7 +133,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'saleh'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
